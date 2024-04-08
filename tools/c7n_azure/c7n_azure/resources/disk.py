0235d1cbf8f815fba0b6a2560789cba68ef24539
Dmytro Afanasiev <dmytro_afanasiev1@epam.com>
2024-04-08 16:29:15 +0300

    Add snapshots filter to azure.disk

--- tools/c7n_azure/c7n_azure/resources/disk.py
+++ tools/c7n_azure/c7n_azure/resources/disk.py
@@ -3,6 +3,8 @@
 
 from c7n_azure.resources.arm import ArmResourceManager
 from c7n_azure.provider import resources
+from c7n.filters import ListItemFilter
+from c7n.utils import type_schema
 
 
 @resources.register('disk')
@@ -39,3 +41,35 @@ class Disk(ArmResourceManager):
             'sku.name'
         )
         resource_type = 'Microsoft.Compute/disks'
+
+
+@Disk.filter_registry.register("snapshots")
+class DiskSnapshotsFilter(ListItemFilter):
+    schema = type_schema(
+        "snapshots",
+        attrs={"$ref": "#/definitions/filters_common/list_item_attrs"},
+        count={"type": "number"},
+        count_op={"$ref": "#/definitions/filters_common/comparison_operators"}
+    )
+    annotate_items = True
+    item_annotation_key = "c7n:Snapshots"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._snapshots = ()
+
+    def process(self, resources, event=None):
+        self._snapshots = tuple(
+            item.serialize(True)
+            for item in self.manager.get_client().snapshots.list()
+        )
+        return super().process(resources, event)
+
+    def get_item_values(self, resource):
+        uid = resource['properties']['uniqueId']
+        filtered = []
+        for item in self._snapshots:
+            source_uid = item['properties'].get('creationData', {}).get('sourceUniqueId')
+            if source_uid == uid:
+                filtered.append(item)
+        return filtered
--- tools/c7n_azure/tests_azure/tests_resources/test_disk.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_disk.py
@@ -1,6 +1,9 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
+import datetime
 from ..azure_common import BaseTest, arm_template
+from c7n.testing import mock_datetime_now
+from dateutil.parser import parse as date_parse
 
 
 class DiskTest(BaseTest):
@@ -28,3 +31,26 @@ class DiskTest(BaseTest):
         })
         resources = p.run()
         self.assertEqual(len(resources), 1)
+
+    @arm_template('disk.json')
+    def test_disk_without_snapshots(self):
+        p = self.load_policy({
+            'name': 'test-disk-with-snapshots-within-14-days',
+            'resource': 'azure.disk',
+            'filters': [{
+                'type': 'snapshots',
+                'attrs': [{
+                    'type': 'value',
+                    'key': 'properties.timeCreated',
+                    'op': 'le',
+                    'value': 14,
+                    'value_type': 'age'
+                }]
+            }]
+        })
+        with mock_datetime_now(date_parse('2021/04/01 00:00'), datetime):
+            resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual('cctestvm_disk2_a9097edcfa664ff48c8e88e87d72003e',
+                         resources[0]['name'])
+

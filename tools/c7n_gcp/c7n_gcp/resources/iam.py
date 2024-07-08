bc7e187863a89deafc2af45f7927935526b75864
Dmytro Afanasiev <dmytro_afanasiev1@epam.com>
2024-07-08 14:53:37 +0300

    Add gcp.service-account-bindings

--- tools/c7n_gcp/c7n_gcp/resources/iam.py
+++ tools/c7n_gcp/c7n_gcp/resources/iam.py
@@ -7,6 +7,9 @@ from c7n_gcp.filters.iampolicy import IamPolicyFilter
 from c7n_gcp.provider import resources
 from c7n_gcp.query import QueryResourceManager, TypeInfo, ChildResourceManager, ChildTypeInfo
 from c7n_gcp.actions import MethodAction
+from c7n.filters import Filter
+from c7n.filters.core import op
+from c7n.utils import local_session
 
 
 @resources.register('project-role')
@@ -67,6 +70,73 @@ class ServiceAccount(QueryResourceManager):
         @staticmethod
         def get_metric_resource_name(resource):
             return resource["uniqueId"]
+
+
+@resources.register('service-account-bindings')
+class ServiceAccountBindings(ChildResourceManager):
+    class resource_type(ChildTypeInfo):
+        service = 'iam'
+        version = 'v1'
+        component = 'projects.serviceAccounts'
+        enum_spec = ('getIamPolicy', 'bindings[]', None)
+        scope = 'resource'
+        scope_key = 'resource'
+        id = name = 'name'
+        default_report_fields = [name, 'description']
+        parent_spec = {
+            'resource': 'service-account',
+            'child_enum_params': {
+                ('name', 'resource')},
+        }
+
+
+@ServiceAccountBindings.filter_registry.register('new-roles-iam-filter')
+class NewRolesIAMFilter(Filter):
+    schema = type_schema('new-roles-iam-filter',
+                         op={'$ref': '#/definitions/filters_common/value'},
+                         value={'$ref': '#/definitions/filters_common/value'},
+                         by={'$ref': '#/definitions/filters_common/value'})
+    permissions = ('resourcemanager.projects.list',)
+
+    def process(self, resources, event=None):
+        filtered = []
+        session = local_session(self.manager.session_factory)
+        client_simple = session.client(service_name='iam', version='v1', component='roles')
+        client_custom = session.client(service_name='iam', version='v1', component='projects.roles')
+        by_who = self.data.get('by')
+
+        for resource in resources:
+            for member in resource['members']:
+                if by_who == 'user' and member.startswith(by_who):
+                    if resource['role'].startswith('project'):
+                        permissions = client_custom.execute_command('get', {
+                            "name": resource['role']})['includedPermissions']
+                        if op(self.data, permissions, self.data.get('value')):
+                            filtered.append(resource)
+                            break
+                    else:
+                        permissions = client_simple.execute_command('get', {
+                            "name": resource['role']})['includedPermissions']
+                        if op(self.data, permissions, self.data.get('value')):
+                            filtered.append(resource)
+                            break
+
+                elif by_who == 'serviceAccount' and member.startswith(by_who):
+                    if resource['role'].startswith('project'):
+                        permissions = client_custom.execute_command('get', {
+                            "name": resource['role']})['includedPermissions']
+                        if op(self.data, permissions, self.data.get('value')):
+                            filtered.append(resource)
+                            break
+                    else:
+                        permissions = client_simple.execute_command('get', {
+                            "name": resource['role']})['includedPermissions']
+                        if op(self.data, permissions, self.data.get('value')):
+                            filtered.append(resource)
+                            break
+                else:
+                    continue
+        return filtered
 
 
 @ServiceAccount.action_registry.register('delete')

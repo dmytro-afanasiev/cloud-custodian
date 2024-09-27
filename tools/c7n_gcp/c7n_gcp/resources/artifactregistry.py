45d9ed3aa4e4757f42151188471ae681d5bd6ff1
Dmytro Afanasiev <dmytro_afanasiev1@epam.com>
2024-09-27 16:09:52 +0300

    Use threads

--- tools/c7n_gcp/c7n_gcp/resources/artifactregistry.py
+++ tools/c7n_gcp/c7n_gcp/resources/artifactregistry.py
@@ -1,6 +1,9 @@
+import os
+
 from c7n.utils import local_session
 from c7n_gcp.provider import resources
 from c7n_gcp.query import ChildResourceManager, ChildTypeInfo
+from concurrent.futures import as_completed
 
 
 @resources.register('artifact-repository')
@@ -33,4 +36,35 @@ class ArtifactRegistryRepository(ChildResourceManager):
             )
         }
 
-    # todo threads?
+    def _get_children(self, query):
+        return super(ChildResourceManager, self)._fetch_resources(query)
+
+    def _fetch_resources(self, query):
+        if not query:
+            query = {}
+        annotation_key = self.resource_type.get_parent_annotation_key()
+        parent_query = self.get_parent_resource_query()
+        parent_resource_manager = self.get_resource_manager(
+            resource_type=self.resource_type.parent_spec['resource'],
+            data=({'query': parent_query} if parent_query else {})
+        )
+        result = []
+        parent_instances = parent_resource_manager.resources()
+        with self.executor_factory(None if os.name == 'nt' else len(parent_instances)) as w:
+            futures = {}
+            for parent_instance in parent_instances:
+                query.update(self._get_child_enum_args(parent_instance))
+                futures[w.submit(self._get_children, query)] = parent_instance
+            for f in as_completed(futures):
+                exc = f.exception()
+                if exc:
+                    self.log.error(
+                        "Exception getting artifacts repositories by location \n %s" % (
+                            exc)
+                    )
+                    continue
+                children = f.result()
+                for child_instance in children:
+                    child_instance[annotation_key] = futures[f]
+                result.extend(children)
+        return result

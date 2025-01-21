d20474db8653f2e453c6e920dd42a3893fc975c1
Dmytro Afanasiev <dmytro_afanasiev1@epam.com>
2025-01-21 12:04:26 +0200

    Add vnet network interface tests

--- tools/c7n_azure/tests_azure/tests_resources/test_vnet.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_vnet.py
@@ -1,8 +1,22 @@
 from ..azure_common import BaseTest
 
 
-class SubnetApplicationGatewayTest(BaseTest):
+class VnetNetworkInterfaceFilter(BaseTest):
 
-    def test_vnet_network_interface_assignment_filter(self):
-        # TODO: fix
-        pass
+    def test_vm_network_interface(self):
+        p = self.load_policy({
+            'name': 'test-vnet-network-interface',
+            'resource': 'azure.vnet',
+            'filters': [{
+                'type': 'network-interface',
+                'attrs': [{
+                    'type': 'value',
+                    'key': 'properties.virtualMachine',
+                    'value': 'present'
+                }]
+            }]
+        })
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]['name'], 'vnet1-141asbfwroute')
+        self.assertEqual(len(resources[0]['c7n:NetworkInterfaces']), 1)
